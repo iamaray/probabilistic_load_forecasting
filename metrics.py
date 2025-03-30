83a9bf27ca97eecd0e3fca9e96f48ef37bde2c1c
iamaray <arya.nahri@gmail.com>
2025-03-29 20:04:35 -0400

    cleanup to metrics code

--- metrics.py
+++ metrics.py
@@ -1,7 +1,36 @@
 import numpy as np
 import torch
 
-def compute_metrics(forecasts, ground_truth, train_scaler=None, alpha=0.2):
+
+def compute_acr_ail(forecasts, ground_truth, alpha=0.2):
+    # average coverage rate and average interval length
+    lower_bound = np.percentile(forecasts, 100 * (alpha / 2), axis=-1)
+    upper_bound = np.percentile(forecasts, 100 * (1 - alpha / 2), axis=-1)
+
+    interval_length = upper_bound - lower_bound
+    in_interval = (ground_truth >= lower_bound) & (ground_truth <= upper_bound)
+    return np.mean(in_interval), np.mean(interval_length)
+
+
+def compute_aes(forecasts, ground_truth):
+    # average energy score
+    batch_size, num_hours, num_variates, num_samples = forecasts.shape
+    forecasts_flat = forecasts.reshape(-1, num_samples)
+    ground_truth_flat = ground_truth.reshape(-1, 1)
+
+    term1 = np.mean(np.linalg.norm(forecasts_flat - ground_truth_flat, axis=1))
+
+    # ||F - F'||_2 (mean pairwise distance between forecast samples)
+    pairwise_diffs = np.sqrt(
+        np.sum((forecasts_flat[:, :, None] -
+               forecasts_flat[:, None, :]) ** 2, axis=1)
+    )
+    term2 = np.mean(pairwise_diffs)
+
+    return term1 - 0.5 * term2
+
+
+def compute_metrics(forecasts, ground_truth, alpha=0.2):
     """
     Compute metrics for probabilistic forecasts:
     - 80% confidence interval
@@ -27,29 +56,9 @@ def compute_metrics(forecasts, ground_truth, train_scaler=None, alpha=0.2):
         raise ValueError(f"Forecasts shape {
                          forecasts.shape[:-1]} and ground truth shape {ground_truth.shape} are not aligned.")
 
-    lower_bound = np.percentile(forecasts, 100 * (alpha / 2), axis=-1)
-    upper_bound = np.percentile(forecasts, 100 * (1 - alpha / 2), axis=-1)
-
-    interval_length = upper_bound - lower_bound
-    avg_interval_length = np.mean(interval_length)
-
-    in_interval = (ground_truth >= lower_bound) & (ground_truth <= upper_bound)
-    avg_coverage_rate = np.mean(in_interval)
-
-    batch_size, num_hours, num_variates, num_samples = forecasts.shape
-    forecasts_flat = forecasts.reshape(-1, num_samples)
-    ground_truth_flat = ground_truth.reshape(-1, 1)
-
-    term1 = np.mean(np.linalg.norm(forecasts_flat - ground_truth_flat, axis=1))
-
-    # ||F - F'||_2 (mean pairwise distance between forecast samples)
-    pairwise_diffs = np.sqrt(
-        np.sum((forecasts_flat[:, :, None] -
-               forecasts_flat[:, None, :]) ** 2, axis=1)
-    )
-    term2 = np.mean(pairwise_diffs)
-
-    energy_score = term1 - 0.5 * term2
+    avg_coverage_rate, avg_interval_length = compute_acr_ail(
+        forecasts, ground_truth, alpha)
+    energy_score = compute_aes(forecasts, ground_truth)
 
     # Return results
     return {
